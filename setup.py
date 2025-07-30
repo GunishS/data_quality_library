a371eb0621b87b9dd83f9454869860c8a4415081
GunishS <gunish99@gmail.com>
2025-07-31 00:09:48 +0530

    putting the library into pypi

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ setup(
     version="0.1.0",
     description="A Python library for data quality checks for different layers that can be intregated and used with multiple artifacts like data pipeline, notebook etc.",
     author="Gunish Swarnkar",
+    author_email="gunish99@gmail.com",
     packages=find_packages(),
     install_requires=[
         "pyspark"
     ],
-    keywords=['data quaity','data testing','quality of data']
+    keywords=['data quaity','data testing','quality of data'],
     python_requires=">=3.7",
     classifiers=[
-        "Development Status :: Testing",
-        "Intented Audience :: Developers"
+        "Development Status :: 4 - Beta"
+        # "Intented Audience :: Developers"
     ]
 )
